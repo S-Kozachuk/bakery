d3df12caba7d35ab0310bda3c2ede210d773ea16
S-Kozachuk <kozachuk-dev@yandex.ru>
2021-11-26 15:50:48 +0300

    NAV / Adaptive. Added script to active .noscroll

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   const menuToggle = document.querySelector('.toggle-menu');
   const mobMenu = document.querySelector('.nav-menu-mobile');
   const overlayEl = document.querySelector('#overlay');
+  const bodyEl = document.body;
   menuToggle.addEventListener('click', function() {
     this.classList.toggle('active');
     mobMenu.classList.toggle('active');
     overlayEl.classList.toggle('active');
+    bodyEl.classList.toggle('noscroll');
   });
 });
 
